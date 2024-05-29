1c7a5e07e35cf2365fa7c2af62eda3a243c63e2e
SanilSane <sanilsane333@gmail.com>
2024-05-29 09:50:48 +0530

    REVIEW MS

---
+++ src/main/java/com/embarkx/reviewms/review/ReviewService.java
@@ -0,0 +1,17 @@
+package com.embarkx.reviewms.review;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    List<Review> getAllReviews(Long companyId);
+
+    boolean addReview(Long companyId,Review review);
+
+
+    Review getReviewById( Long reviewId );
+
+    boolean updateReview( Long reviewId, Review review);
+
+    boolean deleteReview( Long reviewId);
+}
